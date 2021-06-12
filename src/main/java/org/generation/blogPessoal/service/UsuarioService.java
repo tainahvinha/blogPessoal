25a9db29df53705133ff88570908f8de6d1aee43
tainah vinha <tainah_vinha@hotmail.com>
2021-06-11 23:25:16 -0300

    deploy no heroku

---
+++ src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -0,0 +1,50 @@
+package org.generation.blogPessoal.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+import org.apache.commons.codec.binary.Base64;
+
+import org.generation.blogPessoal.model.UserLogin;
+import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	public Usuario cadastrarUsuario(Usuario usuario) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+
+		return repository.save(usuario);
+
+	}
+
+	public Optional<UserLogin> Logar(Optional<UserLogin> usuarioLogin) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuarioBD = repository.findByUsuario(usuarioLogin.get().getUsuario());
+
+		if (usuarioBD.isPresent()) {
+			if (encoder.matches(usuarioLogin.get().getSenha(), usuarioBD.get().getSenha())) {
+
+				String auth = usuarioLogin.get().getUsuario() + ":" + usuarioLogin.get().getSenha();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodedAuth);
+
+				usuarioLogin.get().setToken(authHeader);
+				usuarioLogin.get().setNome(usuarioBD.get().getNome());
+				return usuarioLogin;
+			}
+		}
+		return null;
+
+	}
+
+}
